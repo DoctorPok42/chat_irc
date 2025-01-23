9b6d694a8b21db3a45b4d06ff902a564a5442e2f
Emilien <emilien.dewilde@epitech.eu>
2025-01-23 10:15:44 +0100

    feat -> add function: create/delete channel, list users/channel, private msg

--- second_version/src/index.ts
+++ second_version/src/index.ts
@@ -5,6 +5,7 @@ import connectDB from './db';
 import User from './models/user';
 import Message from './models/message';
 import jwt from 'jsonwebtoken';
+import Channel from './models/channels';
 
 const JWT_SECRET = 'secret';
 
@@ -83,30 +84,147 @@ io.on('connection', (socket: Socket) => {
 
     });
 
-    socket.on('joinChannel', async (data) => {
+    socket.on('joinChannel', async (data, callback) => {
         try {
             const { channel, username } = data;
             const user = await User.findOne({username});
             if (user?.channels.includes(channel)) {
-                console.log('User already in channel');
+                console.log(`${username} already in channel`);
                 return;
             }
             socket.join(channel);
-            console.log(`User joined channel ${channel}`);
+            await Channel.findOneAndUpdate({name: channel}, {$push: {users: username}});
+            console.log(`${username} joined channel ${channel}`);
             if (user != null) {
                 user.channels.push(channel);
                 await user.save();
             }
+            callback({ success: true, messages: await Message.find({ channel })});
             socket.to(channel).emit('message', { text: `${username} join ${channel}`, sender: 'server', timestamp: new Date(), channel: channel });
         } catch (error) {
             console.error(error);
         }
         });
-    socket.on('leaveChannel', (data) => {
-        const { channel, username } = data;
-        socket.leave(channel);
-        console.log(`User left channel ${channel}`);
-        socket.to(channel).emit('message', { text: `${username} left ${channel}`, sender: 'server', timestamp: new Date(), channel });
+
+    socket.on('leaveChannel', async (data, callback) => {
+        try{
+            const { channel, username } = data;
+            const user = await User.findOne({username});
+            if(!user?.channels.includes(channel)){
+                callback({success: false, message: 'User not in channel'});
+                return;
+            }
+            socket.leave(channel);
+            await Channel.findOneAndUpdate({name: channel}, {$pull: {users: username}});
+            await User.findOneAndUpdate({username}, {$pull: {channels: channel}});
+            callback({success: true, message: 'Channel left'});
+            console.log(`${username} left channel ${channel}`);
+            socket.to(channel).emit('message', { text: `${username} left ${channel}`, sender: 'server', timestamp: new Date(), channel });
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    socket.on("listUsers", async (data, callback) => {
+        const { channel } = data;
+        try {
+            const users = await User.find({channels: channel});
+            callback(users.map((user) => user.username));
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    socket.on('changeNickname', async (data, callback) => {
+        const { username, newUsername } = data;
+        try {
+            const user = await User.findOneAndUpdate({ username }, { username: newUsername }, { new: true });
+            if (user) {
+                callback({ success: true, message: 'Nickname changed' });
+            } else {
+                callback({ success: false, message: 'User not found' });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    socket.on('listChannels', async (data, callback) => {
+        const {username} = data;
+        try {
+            const user = await User.findOne({username});
+            if (user){
+                callback({success: true, channels : user.channels});
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    socket.on('createChannel', async (data, callback) => {
+        const { channel } = data;
+        try {
+            const existingChannel = await Message.findOne({ channel });
+            if (existingChannel) {
+                callback({ success: false, message: 'Channel already exists' });
+                return;
+            }
+            const newChannel = new Channel({ name: channel, users: [] });
+            await newChannel.save();
+            callback({ success: true, message: 'Channel created' });
+        } catch (error) {
+            console.error(error);
+        };
+    });
+
+    socket.on('deleteChannel', async (data, callback) => {
+        const { channel } = data;
+        try {
+            const existingChannel = await Channel.findOne({ name: channel });
+            if(!existingChannel) {
+                callback({ success: false, message: 'Channel does not exist' });
+                return;
+            }
+            for (const user of existingChannel.users) {
+                await User.findOneAndUpdate({ username: user }, { $pull: { channels: channel } });
+            }
+            await Channel.deleteOne({ name: channel });
+            callback({ success: true, message: 'Channel deleted' });
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    socket.on('privateMessage', async (data, callback) => {
+        const { sender, receiver, text } = data;
+        try {
+            const receiverUser = await User.findOne({ username: receiver });
+            if (!receiverUser) {
+                callback({ success: false, message: 'Receiver not found' });
+                return;
+            }
+            const existingChannel = await Channel.findOne({
+                $or: [
+                    { name: `${sender}-${receiver}` },
+                    { name: `${receiver}-${sender}` },
+                ],
+            });
+            let channelName: string;
+            if (!existingChannel) {
+                channelName = `${sender}-${receiver}`;
+                const newChannel = new Channel({ name: channelName, users: [sender, receiver] });
+                await newChannel.save();
+            } else {
+                channelName = existingChannel.name;
+            }
+            const message = new Message({ text: text, sender: sender, timestamp: new Date(), channel: channelName });
+            await message.save();
+            socket.broadcast.to(channelName).emit('message', { text, sender, timestamp: new Date(), channel: channelName });
+            callback({ success: true, message: 'Message sent' });
+        } catch (error) {
+            console.error(error);
+            callback({ success: false, message: 'Server error' });
+        }
     });
 
 
